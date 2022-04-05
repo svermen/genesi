03c683d294e4ceb2deb43373bf79afdd65129f14
Santiago Vermeulen <48023484+svermen@users.noreply.github.com>
2022-04-05 14:18:20 -0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,47 @@
 import '../styles/globals.css'
+import '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 import Link from 'next/link'
 import Image from "next/image";
 import logo  from "../public/images/logo.png";
+import React, { useEffect } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Web3 Browswer Detection
+    if (typeof window.ethereum !== "undefined") {
+      console.log("Injected Web3 Wallet is installed!");
+    }
+
+    //Button ID
+    const connectButton = document.getElementById("connect");
+
+    //Click Event
+    connectButton.addEventListener("click", () => {
+      connectAccount();
+    });
+
+    //Connect Account Function
+    async function connectAccount() {
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const account = accounts[0];
+      connectButton.innerHTML =
+        account[0] +
+        account[1] +
+        account[2] +
+        account[3] +
+        account[4] +
+        account[5] +
+        "..." +
+        account[38] +
+        account[39] +
+        account[40] +
+        account[41];
+    }
+  }, []);
   return (
     <div style={{
       backgroundColor: 'gray'
@@ -38,11 +76,17 @@ function MyApp({ Component, pageProps }) {
               Doc
             </a>
           </Link>
-          <Link href="/creator-dashboard">
+          {/* <Link href="/creator-dashboard">
             <a className="block py-2 pl-3 pr-4 text-2xl font-bold text-base border-b border-gray-100 hover:bg-gray-50 cursor-pointer md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
               Wallet
             </a>
-          </Link>
+          </Link> */}
+           
+      
+      <button className={styles.connect} id="connect">
+        Connect Wallet
+      </button>
+   
          </ul>
          </div>
         </div>
